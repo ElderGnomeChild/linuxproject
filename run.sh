681d158658695597082e33880a418102cfbc1ef4
Daniel Lier <daniellier12@gmail.com>
2018-11-01 11:20:27 -0600

    FINAL COMMIT (I hope) Everything works, uncommented the line to make directory A.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd /
 cd var/www/html
-#sudo mkdir A
+sudo mkdir A
 sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> Welcome! </h1> <p> Click <a href="file:///var/www/html/A/A.html">here</a> to go to the second file. </p> </body> </html>" >> index.html'
 cd A
 sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> This is the second one. </h1> <p> We made it. </p> </body> </html>" >> A.html'
