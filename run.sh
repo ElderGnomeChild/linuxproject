da2aa26a98ff5d91fef12c1f95141efa60c44a5a
Daniel Lier <daniellier12@gmail.com>
2018-11-01 10:58:35 -0600

    creates an html file, no link, no second html file, does not make directory A because it already exists

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 cd /
 cd var/www/html
-sudo mkdir A
-echo <!DOCTYPE html> <html> <body> <h1> Welcome! </h1> <p> Hello! </p> </body> </html> >> index.html
+#sudo mkdir A
+#index.html chmod 755
+sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> Welcome! </h1> <p> Hello! </p> </body> </html>" >> index.html'
+
