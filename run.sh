624cb7b00f411b1a60ac80ceb47655e594bf3abb
Daniel Lier <daniellier12@gmail.com>
2018-11-01 11:10:04 -0600

    final commit. link works. sh file now makes both files

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ cd /
 cd var/www/html
 #sudo mkdir A
 #index.html chmod 755
-sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> Welcome! </h1> <p> Hello! </p> </body> </html>" >> index.html'
-
+sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> Welcome! </h1> <p> Click <a href="file:///var/www/html/A/A.html">here</a> to go to the second file. </p> </body> </html>" >> index.html'
+cd A
+sudo bash -c 'echo "<!DOCTYPE html> <html> <body> <h1> This is the second one. </h1> <p> We made it. </p> </body> </html>" >> A.html'
